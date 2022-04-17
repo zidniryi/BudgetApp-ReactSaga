c8605e499945dd22490049b4a073dfcfddcf6b74
zidniryi <zidnirydwan@gmail.com>
2022-04-17 20:20:29 +0700

    10 2

--- src/hooks/useEntry.js
+++ src/hooks/useEntry.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { addEntryRedux, updateEntryRedux } from "../actions/entries.action";
 import { v4 as uuidv4 } from "uuid";
