535b13d52d4cb12404498d6b171010fe91b8671d
zidniryi <zidnirydwan@gmail.com>
2022-04-20 22:29:25 +0700

    15 2

--- src/sagas/entries.saga.js
+++ src/sagas/entries.saga.js
@@ -1,19 +1,18 @@
 import axios from 'axios';
 import { call, fork, put, take } from 'redux-saga/effects';
 
-import types, { populateEntries } from '../actions/entries.action';
+import types, { populateEntries, populateEntryDetail } from '../actions/entries.action';
 
 export function* getAllEntries() {
     yield take(types.GET_ENTRIES)
     const { data } = yield call(axios, 'http://localhost:3011/entries')
-    yield put(
-        populateEntries(data))
+    yield put(populateEntries(data))
 }
 
 export function* getEntryDetails(id) {
     const result = yield call(axios, `http://localhost:3011/values/${id}`)
     console.log(result.data)
-    yield put({ type: types.POPULATE_ENTRIES_DETAIL, payload: { id, entry: result.data } })
+    yield put(populateEntryDetail(id, result.data))
 }
 
 export function* getAllEntriesDetails() {
