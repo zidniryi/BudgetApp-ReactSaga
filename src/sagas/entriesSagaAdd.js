17dcd6ac1becf307b64a0249b30c0c2407e2392e
zidniryi <zidnirydwan@gmail.com>
2022-04-22 21:22:40 +0700

    Setup POST with Saga

---
+++ src/sagas/entriesSagaAdd.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+import { call, put, take } from 'redux-saga/effects';
+import types from '../actions/entries.action';
+
+export function* addEntrySaga() {
+
+}
--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,11 +1,12 @@
 // import * as testSaga from './testSaga';
 import * as entriesSaga from './entries.saga';
 import * as entrySagaDeletion from './entriesSagaDeletetion'
+import * as entrySagaAdd from './entriesSagaAdd'
 
 
 export function initSagas(sagaMiddleware) {
     // Object.values(testSaga).forEach(sagaMiddleware.run.bind())
     Object.values(entriesSaga).forEach(sagaMiddleware.run.bind())
     Object.values(entrySagaDeletion).forEach(sagaMiddleware.run.bind())
-
+    Object.values(entrySagaAdd).forEach(sagaMiddleware.run.bind())
 }
